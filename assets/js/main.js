6551cb299c7fc584911adf334e7f9bf66aadc5fc
n0rdlyss <malioposum@gmail.com>
2019-03-24 19:51:54 +0100

    Change poptrox with Magnific Popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,25 @@
-/*
-	Visualize by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 $(function() {
+		$('body').addClass('is-loading');
 
-	// Vars.
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper');
+		$(document).ready(function () {
+			$('body').removeClass('is-loading');
 
-	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
-
-	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
-
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
-
-	// Poptrox.
-		$window.on('load', function() {
-
-			$('.thumbnails').poptrox({
-				onPopupClose: function() { $body.removeClass('is-covered'); },
-				onPopupOpen: function() { $body.addClass('is-covered'); },
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: true,
-				overlayColor: '#000000',
-				overlayOpacity: 0.75,
-				popupLoaderText: '',
-				fadeSpeed: 500,
-				usePopupDefaultStyling: false,
-				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+			$('.thumbnails').magnificPopup({
+				delegate: 'a',
+				type: 'image',
+				removalDelay: 500,
+				callbacks: {
+					beforeOpen: function () {
+						this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+						this.st.mainClass = 'mfp-move-from-top';
+					}
+				},
+				closeOnContentClick: true,
+				enableEscapeKey: true,
+				midClick: true,
+				image: {
+					verticalFit: false
+				}
 			});
-
 		});
-
 });
